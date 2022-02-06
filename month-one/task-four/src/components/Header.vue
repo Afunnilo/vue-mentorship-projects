86a8baba071642ea0cfb4e601de4d955f4243b6f
Afunnilo <afunnilo@gmail.com>
2022-02-06 21:48:36 +0100

    task one completed

---
+++ month-one/task-four/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='header'>
+      <p class='headerText'>devfinder</p>
+      <p>light <i class="fas fa-sun"></i></p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+.header{
+    display:flex;
+    justify-content: space-between;
+}
+</style>
